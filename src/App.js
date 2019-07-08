599f793e9f85d66c0642d0358a2483805d98cb68
Matheus <matheuslopesfrazao@outlook.com>
2019-07-08 10:23:42 -0300

    Componente 'Portfólio' finalizado

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Cabecalho from './componentes/Cabecalho'
 import Inicio from './componentes/Inicio'
 import Servicos from './componentes/Servicos'
+import Portfolio from './componentes/Portfolio'
 import Site from './site'
 import Rodape from './componentes/Rodape'
 
@@ -15,6 +16,7 @@ class App extends Component {
           <Cabecalho />
           <Route path="/" exact component={Inicio} /> {/* o "exact" quer dizer que essa rota só vai ser ativada, se o link for exatamente "/" */}
           <Route path="/servicos" component={Servicos} />
+          <Route path="/portfolio" component={Portfolio} />
           <Servicos />
           <Rodape />
         </div>
--- src/componentes/Cabecalho.js
+++ src/componentes/Cabecalho.js
@@ -17,8 +17,8 @@ const Cabecalho = props => {
                     <div className='collapse navbar-collapse' id='myNavbar'>
                         <ul className='nav navbar-nav navbar-right'>
                             <li><Link to="/">INICIO</Link></li>
-                            <li><Link to="/Servicos">SERVIÇOS</Link></li>
-                            <li><a href='#portfolio'>PORTFOLIO</a></li>
+                            <li><Link to="/servicos">SERVIÇOS</Link></li>
+                            <li><Link to='/portfolio'>PORTFOLIO</Link></li>
                             <li><a href='#pricing'>PRICING</a></li>
                             <li><a href='#contact'>CONTACT</a></li>
                         </ul>
