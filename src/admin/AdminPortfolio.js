a2828d501f5425a45d864aaa1134d7184d8cf285
Matheus <matheuslopesfrazao@outlook.com>
2019-07-26 21:02:43 -0300

    Criando o formulário de cadastro de portfólio

--- src/admin/AdminPortfolio.js
+++ src/admin/AdminPortfolio.js
@@ -1,13 +1,32 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
+
+import config, { storage } from './../firebase-config'
 
 class AdminPortfolio extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
     }
 
-    render(){
-        return(
-            <h2>Portfolio - Área administrativa</h2>
+    render() {
+        return (
+            <div className="container">
+                <h2>Portfolio - Área administrativa</h2>
+                <form>
+                    <div className="form-group">
+                        <label for="titulo">Título</label>
+                        <input type="text" className="form-control" id="titulo" placeholder="Seu email" />
+                    </div>
+                    <div className="form-group">
+                        <label for="descricao">Descrição</label>
+                        <textarea class="form-control" id="descricao" rows="3"></textarea>
+                    </div>
+                    <div className="form-group">
+                        <label for="Imagem">Imagem</label>
+                        <input type="file" class="form-control-file" id="imagem" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Salvar</button>
+                </form>
+            </div>
         )
     }
 }
