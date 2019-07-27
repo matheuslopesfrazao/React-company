86935a3532035753f9d0a8a38ef352001c8543a4
Matheus <matheuslopesfrazao@outlook.com>
2019-07-26 23:40:09 -0300

    Formulário da página de Portfólio preparado para gravar no BD

--- src/admin/AdminPortfolio.js
+++ src/admin/AdminPortfolio.js
@@ -5,24 +5,37 @@ import config, { storage } from './../firebase-config'
 class AdminPortfolio extends Component {
     constructor(props) {
         super(props)
+
+
+
+        this.gravaPortfolio = this.gravaPortfolio.bind(this)
+    }
+
+    gravaPortfolio(e){
+        console.log('Vamos gravar o portfolio aí mano?!')
+        console.log(this.titulo.value)
+        console.log(this.descricao.value)
+        console.log(this.imagem.value)
+
+        e.preventDefault() // Bloqueia o refresh da página depois de clicar no botão submit (Botão salvar)
     }
 
     render() {
         return (
             <div className="container">
                 <h2>Portfolio - Área administrativa</h2>
-                <form>
+                <form onSubmit={this.gravaPortfolio}>
                     <div className="form-group">
-                        <label for="titulo">Título</label>
-                        <input type="text" className="form-control" id="titulo" placeholder="Seu email" />
+                        <label htmlFor="titulo">Título</label>
+                        <input type="text" className="form-control" id="titulo" placeholder="Seu email" ref={(ref) => this.titulo = ref} />
                     </div>
                     <div className="form-group">
-                        <label for="descricao">Descrição</label>
-                        <textarea class="form-control" id="descricao" rows="3"></textarea>
+                        <label htmlFor="descricao">Descrição</label>
+                        <textarea className="form-control" id="descricao" rows="3" ref={(ref) => this.descricao = ref}></textarea>
                     </div>
                     <div className="form-group">
-                        <label for="Imagem">Imagem</label>
-                        <input type="file" class="form-control-file" id="imagem" />
+                        <label htmlFor="Imagem">Imagem</label>
+                        <input type="file" className="form-control-file" id="imagem" ref={(ref) => this.imagem = ref} />
                     </div>
                     <button type="submit" className="btn btn-primary">Salvar</button>
                 </form>
