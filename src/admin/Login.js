39a7b90dc0bbbdeb6b32ff8a9a08a27331a61846
Matheus <matheuslopesfrazao@outlook.com>
2019-07-30 20:23:58 -0300

    Conectando o form de login

--- src/admin/Login.js
+++ src/admin/Login.js
@@ -3,25 +3,28 @@ import React, { Component } from 'react'
 class Login extends Component {
     constructor(props) {
         super(props)
+
+        this.email = null
+        this.senha = null
+
+        this.autenticaUsuario = this.autenticaUsuario.bind(this)
     }
 
 
     render() {
         return (
             <div className="container">
-                <h1>Login</h1>
-                <form>
+                <h1>Login</h1>                
                     <div className="form-group">
                         <label for="exampleInputEmail1">Endereço de email</label>
                         <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Seu email" />
-                        <small id="emailHelp" className="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
+                        <small id="emailHelp" name="email" ref={ref => this.email = ref} className="form-text text-muted">Nunca vamos compartilhar seu email, com ninguém.</small>
                     </div>
                     <div className="form-group">
                         <label for="exampleInputPassword1">Senha</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha" />
+                        <input type="password" name="senha" ref={ref => this.senha = ref} class="form-control" id="exampleInputPassword1" placeholder="Senha" />
                     </div>                    
-                    <button type="submit" className="btn btn-primary">Enviar</button>
-                </form>
+                    <button type="button" className="btn btn-primary" onClick={this.autenticaUsuario}>Enviar</button>                
             </div>
                         )
                     }
